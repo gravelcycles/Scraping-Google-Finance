78c1b1e0b474e70134cede97920abbdca817ba5c
David W Grossman <2gotgrossman@gmail.com>
2017-03-25 22:10:01 -0400

    Commented python script, revised runtime errors, and removed csv module dependency

--- Scraping_Google_Finance.py
+++ Scraping_Google_Finance.py
@@ -1,9 +1,9 @@
-
 import pandas as pd
 import urllib2
 import datetime as dt
-import csv
 import os as os
+
+start_stock = 0
 
 path = "/Users/David/Documents/Market_Data_Main/"
 
@@ -15,21 +15,24 @@ stockFiles = set(stockFiles[1:])  # Ignore .DStore file
 stock_by_day_files = os.listdir(path + "Stocks_By_Date")
 stock_by_day_files = set(stock_by_day_files[1:])
 
+
 class NoStockDataError(Exception):
     pass
 
-def get_google_data(symbol, period, window):
+
+def get_google_data(symbol, frequency=60, window=20, exchange='NASDAQ'):
     """
     Function for retrieving data from google.com/finance and then formatting the dataframe
 
     :param symbol: stock ticker
-    :param period: frequency of stock quotes. Minimum: 60 seconds
+    :param frequency: frequency of stock quotes. Minimum: 60 seconds
     :param window: number of days requested
+    :param exchange: Stock Exchange
     :return: dataframe with formatted stock quotes
     """
     url_root = 'http://www.google.com/finance/getprices?i='
-    url_root += str(period) + '&p=' + str(window)
-    url_root += 'd&f=d,o,h,l,c,v&df=cpct&q=' + symbol + '&x=NASDAQ'
+    url_root += str(frequency) + '&p=' + str(window)
+    url_root += 'd&f=d,o,h,l,c,v&df=cpct&q=' + symbol + '&x=' + exchange
     response = urllib2.urlopen(url_root)
     data = response.read().split('\n')
 
@@ -52,7 +55,7 @@ def get_google_data(symbol, period, window):
             continue
         else:
             coffset = int(cdata[0])
-            cts = int(anchor_stamp) + (coffset * period)
+            cts = int(anchor_stamp) + (coffset * frequency)
 
         parsed_data.append([symbol, str(dt.datetime.fromtimestamp(cts)),
                            float(cts), float(cdata[1]),
@@ -60,38 +63,58 @@ def get_google_data(symbol, period, window):
                            float(cdata[4]),  float(cdata[5])])
 
     df = pd.DataFrame(parsed_data)
-    df.columns = ['ticker', 'timestamp', 'unix_time', 'open', 'high', 'low', 'close', 'volume']
+    df.columns = ['company', 'timestamp', 'unix_time', 'open', 'high', 'low', 'close', 'volume']
 
     return df
 
-
+# Keeps track of files from Stocks_By_Date folder that have been used in order
+# to clean up duplicates at the end
 files_opened = set()
 
 
 def add_company_to_day(stock_data):
+    """
+    Splits a company's stock data by day and writes to appropriate day file
+
+    :param stock_data:
+    :return: None
+    """
+
     stock_date_stamp = stock_data['timestamp'].apply(lambda x: x.split(" ")[0])
     unique_dates = stock_date_stamp.unique()
+
     for date in unique_dates:
         date_csv = date + '.csv'
 
-        temp = stock_data[stock_date_stamp == date]
+        # Creates dataframe of date's daya
+        todays_data = stock_data[stock_date_stamp == date]
 
-        with open(path + "Stocks_By_Date/" + date_csv, 'a') as file:
-            if date_csv in stock_by_day_files:
-                temp.to_csv(file, header=False, index=False)
-            else:
-                temp.to_csv(f, header=True, index=False)
+        # If the file already exists, appends data. Otherwise, creates new file
+        if date_csv in stock_by_day_files:
+            todays_data.to_csv(path + "Stocks_By_Date/" + date_csv, header=False, index=False)
+        else:
+            todays_data.to_csv(path + "Stocks_By_Date/" + date_csv, header=True, index=False)
 
         files_opened.add(date_csv)
         stock_by_day_files.add(date_csv)
 
 
 def add_company_to_stock(new_stock_data, company_name):
+    """
+    Adds a company's stock data to an already existing file of data or a new file.
+
+    :param new_stock_data:
+    :param company_name:
+    :return: None
+    """
     company_csv = company_name + '.csv'
     if company_csv in stockFiles:
+
+        # Finds where the new data overlaps with the old data
         old_stock_data = pd.read_csv(path + "Stock_Data/%s" % company_csv)
         first_entry = old_stock_data['timestamp'].iloc(-1)
 
+        # Only appends the non-duplicate data
         if first_entry not in new_stock_data['timestamp']:
             old_stock_data = old_stock_data.append(new_stock_data)
         else:
@@ -99,44 +122,52 @@ def add_company_to_stock(new_stock_data, company_name):
             old_stock_data = old_stock_data.append(new_stock_data[init_index+1:])
 
         old_stock_data.to_csv(path + "Stock_Data/%s" % company_csv, index=False)
+
+    # In the case that the stock does not exist
     else:
         new_stock_data.to_csv(path + "Stock_Data/{}".format(company_csv),
                               index=False)
         stockFiles.add(company_csv)
 
 
+# Gets list of NASDAQ stocks
 companylist = None
-with open(path + 'companylist.csv', 'rb') as csvfile:
-    reader = csv.reader(csvfile, delimiter=',', quotechar='|')
-    companylist = list(reader)
-    companylist = companylist[0]
-    print companylist
+companylist = list(pd.read_csv(path+ 'companylist.csv'))
+print companylist
 
-start = 0
-companylist = companylist[start:1000]
 
-increment = start
+# Sets up iteration over <1001 stocks in order to prevent Google timeout
+if start_stock + 1000 < len(companylist):
+    companylist = companylist[start_stock:1000]
+elif start_stock < len(companylist):
+    companylist = companylist[start_stock:]
+else:
+    raise IndexError
+
+
+increment = start_stock
 missed = []
 for company in companylist:
     try:
+        increment += 1
         company = str(company.strip('"'))
-        company_data = get_google_data(company, 60, 20)
+        company_data = get_google_data(company)
 
         add_company_to_day(company_data)
         add_company_to_stock(company_data, company)
 
-        increment += 1
+
     except urllib2.URLError, e:
         checksLogger.error('URLError = ' + str(e.reason))
+
+    # In the case that Google does not have the stock
     except NoStockDataError, e:
-        increment += 1
+        print increment
         print company
         missed.append(company)
 
-
+# Remove Duplicates for opened Stock_By_Date files
 for file in files_opened:
     current = pd.read_csv(path + "Stocks_By_Date/%s" % file)
     current.drop_duplicates(inplace=True)
     current.to_csv(path + "Stocks_By_Date/%s" % file, index=False)
-
-# sys.exit('hello')
